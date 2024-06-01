b98e9ee023cb0c11e33c5cecb9674619c5bf56b3
Cjxsqa <rdxcj@tutamail.com>
2024-05-31 18:31:22 -0800

    re3

--- main.py
+++ main.py
@@ -209,11 +209,11 @@ print(rr.text)
 RES = re.findall("\d{2,}x([0-9]+)", requests.get(pr).text).index('1080')
 
 print("RESOLTUOOMG : ", RES)
-prin = int(RES)-1
+prin = int(RES)
 print(pr)
 
 #os.system(f"ffmpeg -re -i '{pr}' -vf transpose=1 -acodec copy -f fifo -fifo_format flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 -map 0:v:6 -map 0:a:1 '{upload_url}'")
 
 
-
-os.system(f"ffmpeg -rtbufsize 256M -re -i '{pr}' -vf \"transpose=1,transpose=1,transpose=1,transpose=1\" -threads 4 -map 0:p:{prin} -crf 0 -b:v 10000k -c:a aac -g 30 -b:a 384k -f flv 'rtmp://a.rtmp.youtube.com/live2/qtaa-xx6x-h99h-hjtp-1wf1' -vcodec copy -acodec copy -f flv '{upload_url}'")
+os.system(f"ffmpeg -rtbufsize 256M -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1
+   -reconnect_delay_max 5 -re -i '{pr}' -max_muxing_queue_size 9999 -vf \"transpose=1,transpose=1,transpose=1,transpose=1\" -threads 4 -map 0:p:{prin} -crf 0 -b:v 10000k -c:a aac -g 30 -b:a 384k -f flv 'rtmp://a.rtmp.youtube.com/live2/qtaa-xx6x-h99h-hjtp-1wf1' -vcodec copy -acodec copy -f flv '{upload_url}'")
