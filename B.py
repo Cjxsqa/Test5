4c64cb4014babac29ee5dabef630af0d762a9cb9
Cjxsqa <rdxcj@tutamail.com>
2024-07-07 15:07:27 +0530

    Update B.py

--- B.py
+++ B.py
@@ -111,39 +111,6 @@ headers = {
 }
 
 
-
-import requests
-
-headersd = {
-    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    'sec-ch-ua-platform': '"Linux"',
-    'dnt': '1',
-    'sec-ch-ua-mobile': '?0',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    'content-type': 'application/json',
-    'accept': '*/*',
-    'origin': 'https://dlive.tv',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://dlive.tv/',
-    'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-    'priority': 'u=1, i',
-}
-
-json_dataaa = {
-    'playlisturi': 'https://livestreamb.prdv3.dlivecdn.com/dlive-05900794/1720270925/src/live.m3u8',
-}
-
-responseZ = requests.post('https://live.prd.dlive.tv/hls/sign/url', headers=headersd, json=json_dataaa)
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"playlisturi":"https://livestreamb.prdv3.dlivecdn.com/dlive-05900794/1720270925/src/live.m3u8"}'
-#response = requests.post('https://live.prd.dlive.tv/hls/sign/url', headers=headers, data=data)
-
-
-
 session.headers = headers
 #print(session.headers)
 #session.headers.update({"x-csrftoken": f"{csrf}"})
@@ -193,5 +160,39 @@ rr = session.post(f"https://www.instagram.com/api/v1/live/{broadcastid}/start/",
 #hhhh = "https://fa723fc1b171.us-west-2.playback.live-video.net/api/video/v1/us-west-2.196233775518.channel.uJXcF39rPp9m.m3u8?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzM4NCJ9.eyJhd3M6Y2hhbm5lbC1hcm4iOiJhcm46YXdzOml2czp1cy13ZXN0LTI6MTk2MjMzNzc1NTE4OmNoYW5uZWwvdUpYY0YzOXJQcDltIiwiYXdzOmFjY2Vzcy1jb250cm9sLWFsbG93LW9yaWdpbiI6Imh0dHBzOi8va2ljay5jb20saHR0cHM6Ly9wbGF5ZXIua2ljay5jb20saHR0cHM6Ly9hZG1pbi5raWNrLmNvbSxodHRwczovL3d3dy5nc3RhdGljLmNvbSxodHRwczovLyoua2ljay5saXZlLGh0dHBzOi8vYmV0YS5raWNrLmNvbSIsImF3czpzdHJpY3Qtb3JpZ2luLWVuZm9yY2VtZW50IjpmYWxzZSwiZXhwIjoxNzIwMzI2Mzk0fQ.kIiZGzDOy2shuDSQSVfHHOGP29ZKFyAvDlB5uD64MwUUyk5k4kL_-9eJqWfYEHjxhQtYhTjQmQOw981ghrWTUBLzaNuKMBqXkUaCFrgB5K46fnIGuq1oO2AI_qnrzjqU"
 
 
+headers = {
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-platform': '"Android"',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+    'content-type': 'application/json',
+    'accept': '*/*',
+    'origin': 'https://dlive.tv',
+    'sec-fetch-site': 'same-site',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://dlive.tv/',
+    'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+    'priority': 'u=1, i',
+}
 
-os.system(f"ffmpeg -re -i '{responseZ.text}' -vf transpose=1 -threads 4 -vcodec libx264 -b:v 9000k -acodec copy -preset ultrafast -f flv '{upload_url}'")
+json_data = {
+    'playlisturi': 'https://livestreamb.prdv3.dlivecdn.com/dlive-05900794/1720270925/src/live.m3u8'
+}
+
+esponse44 = requests.post('https://live.prd.dlive.tv/hls/sign/url', headers=headers, json=json_data).text
+print(esponse44)
+
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"playlisturi":"https://livestreamc.prdv3.dlivecdn.com/unitynews/1718128583/src/live.m3u8"}'
+#response = requests.post('https://live.prd.dlive.tv/hls/sign/url', headers=headers, data=data)
+
+
+#os.system("ffmpeg -headers $'User-Agent: Mozilla/5.0 (Android; vivo V2311) Android/14 version/1.17.74\r\nHost: livestreamc.prdv3.dlivecdn.com\r\nConnection: Keep-Alive\r\nAccept-Encoding: identity\r\nReferer: https://dlive.tv/\r\n' -re -i 'https://livestreamc.prdv3.dlivecdn.com/dlive-knfgmgrycm/1718467298/720p/live.m3u8' -vf transpose=1 -c:a copy -preset ultrafast -tune zerolatency -f mpegts -muxrate 7599900 udp://127.0.0.1:47333 | ffmpeg -f mpegts -i udp://127.0.0.1:47333 -vcodec libx264 -acodec copy -preset ultrafast -tune zerolatency -f flv '{upload_url}'")
+
+
+
+
+#os.system(f"ffmpeg -re -i '{responseZ.text}' -vf transpose=1 -threads 4 -vcodec libx264 -b:v 9000k -acodec copy -preset ultrafast -f flv '{upload_url}'")
